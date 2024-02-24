97dafe05b56ea734d31cdfa3fb3ff6ba3119e6ee
Sergio Marin Sanchez <serms1999@gmail.com>
2024-02-24 17:51:31 +0100

    Add exercise 5b

---
+++ basic_exercises/exercise5/b/exercise_04b_closing.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Sergio Marin Sanchez on 24/2/24.
+//
+
+#ifndef IMAGE_PROCESSING_EXERCISE_04B_CLOSING_HPP
+#define IMAGE_PROCESSING_EXERCISE_04B_CLOSING_HPP
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
+// Performs closing of size <size>
+void closing(const cv::Mat *image, cv::Mat *output, u_int size);
+
+#endif //IMAGE_PROCESSING_EXERCISE_04B_CLOSING_HPP
